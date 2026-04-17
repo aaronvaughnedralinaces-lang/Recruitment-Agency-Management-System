3589556cee953146df4cc2dda3d35b06ce3862e5
aaronvaughnedralinaces-lang <aaronvaughn.edralin.aces@gmail.com>
2026-04-17 21:08:02 +0800

    Initial Commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const app = require('./apps');
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+/*app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});*/
+
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
